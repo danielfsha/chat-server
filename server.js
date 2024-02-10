0bbb5b0df081315be79206f01c65207121164576
daniel fisseha <developedbydanielfisseha@gmail.com>
2024-02-10 15:07:24 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+import Gun from "gun";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(Gun.serve);
+
+const PORT = 3000
+
+app.get("/", (request, response) => {
+    response.send("Chat Server is running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listen on the port ${PORT}...`);
+});
+
+Gun({ web: server })
